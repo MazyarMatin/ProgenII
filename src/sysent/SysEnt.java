d75e042e6e2ff4a2ecb8f01b246041a869eaee69
mmt <mmt@x-mm-t.dhcp.fh-bielefeld.de>
2018-05-19 16:36:04 +0200

    Lade Projekt auf GIT hoch.

--- src/sysent/SysEnt.java
+++ src/sysent/SysEnt.java
@@ -17,6 +17,7 @@ public class SysEnt {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hier ist die neue Klasse.");
+        System.out.println("Dies ist eine Laptop Version");
     }
     
 }
