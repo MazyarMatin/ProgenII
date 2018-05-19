b6bf8f79ee3c5440e79e6b96ed1f0c620bbc4383
mmatin <mmatin@9GXCZ4J.ad.fh-bielefeld.de>
2018-05-19 16:20:34 +0200

    Initial Commit 1

--- src/sysent/SysEnt.java
+++ src/sysent/SysEnt.java
@@ -16,6 +16,7 @@ public class SysEnt {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hier ist die neue Klasse.");
     }
     
 }
